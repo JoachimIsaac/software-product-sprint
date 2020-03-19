019d6d9edca44b75a3b890196d600df71884fea3
Joachim <joachimvivian3@gmail.com>
2020-03-18 23:58:14 -0500

    i have about 4 errors remaining

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,136 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Comparator;
+
 
 public final class FindMeetingQuery {
+
+
+
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
-}
+        
+        ArrayList<TimeRange> availableTimes = new ArrayList();
+        
+        ArrayList<Event> eventsCopy = new ArrayList<Event>();
+
+        eventsCopy = loadEvents(events);
+        
+        eventsCopy.sort(Comparator.comparing(Event::getWhen, TimeRange.ORDER_BY_START));
+
+
+        TimeRange tempT = new TimeRange(0, 24 * 60);
+
+    /// if our duration is more than a day 
+        if(request.getDuration() > tempT.duration() ){
+            return availableTimes; // return empty array 
+        }
+        else if(request.getAttendees().size() == 0){
+                 availableTimes.add(tempT);
+                 return availableTimes;
+        }
+        
+        int tempConflict = 0;
+        boolean conflict = false;
+  
+        ArrayList<String> currentAttendees = new ArrayList<String>();
+
+        for(Event eV : eventsCopy){
+            for(String attendee: eV.getAttendees()){
+                if(request.getAttendees().contains(attendee)){
+
+                        if(currentAttendees.contains(attendee)){
+                            conflict = true;
+                            continue;
+                            
+                        }
+
+                        availableTimes.add(getTimeBefore(eV));
+                        tempConflict = eV.getWhen().end();
+                        currentAttendees.add(attendee);
+                }
+                
+            }
+        }
+
+      
+
+        availableTimes.add(getTimeAfter(tempConflict));
+
+ 
+    
+  
+       
+    
+    
+    
+    
+    return availableTimes;
+    
+    }
+
+
+    public static ArrayList<Event> loadEvents(Collection<Event> events){
+         ArrayList<Event> result = new ArrayList<Event>();
+
+        for(Event ev : events){
+            result.add(ev);
+        }
+        return result;
+    }
+
+
+   public static TimeRange getTimeAfter(int start){
+       
+        TimeRange tempT = new TimeRange(start, 24 * 60);
+       tempT = tempT.fromStartEnd(start,(24 * 60) - 1,true);
+       return tempT;
+   }
+
+    public static TimeRange getTimeBefore(Event ev){
+       
+        TimeRange tempT = new TimeRange(0, ev.getWhen().start());
+        
+       return tempT;
+   }
+
+
+    // public static ArrayList<TimeRange> checkIfTimeRangesClash(ArrayList<TimeRange> resquestRanges,Collection<Event> events){
+    //     .
+    //     ArrayList<TimeRange> results = new ArrayList<TimeRange>();
+        
+    //     for(TimeRange rr : resquestRanges){
+            
+    //         int counter = 0; 
+
+       
+    //         for(Event event : events){
+    //             if(event.getWhen().contains(rr)){
+    //                 counter += 1;
+    //                 continue;
+    //             }
+    //         }
+    //     if(counter  == 0 && pt.duration() < pt.END_OF_DAY){
+    //         results.add(rr);
+    //         }
+    //     }
+        
+    //     return results;
+
+    // }
+
+
+  }   
+    
+    
+    
+    
+    
+    
+    
+    
+    
